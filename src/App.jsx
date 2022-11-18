2aa075fc099b3d013b2ccfcf64e4e389a6f3540e
amina <aminakambaralieva.gmail.com>
2022-11-18 20:50:48 +0600

    card

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"
+// import Counter from "./Components/Counter";
+import Card from "./Components/Card"
+
+const App = () => {
+  return (
+    <>
+    <Card />
+      {/* <Counter /> */}
+    </>
+  );
+}
+
+export default App;
